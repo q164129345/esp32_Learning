d66ab1659a2c943311e181eeb1a2b39e47450c05
wallacezhang <wallace.zhang@fjdynamics.com>
2024-05-08 21:41:32 +0800

    UART_ECHO工程跑起来了

--- code_Uart_Echo/main/main.c
+++ code_Uart_Echo/main/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "led.h"
 
 void app_main(void)
 {
-
+    led_Init();       // 初始化LED
+    while(1) {
+        esp_log_write(ESP_LOG_INFO,"MAIN","%s(%d): UART Echo\n",__FUNCTION__, __LINE__);
+        led_Toggle();
+        vTaskDelay(500 / portTICK_PERIOD_MS);    
+    }
 }
