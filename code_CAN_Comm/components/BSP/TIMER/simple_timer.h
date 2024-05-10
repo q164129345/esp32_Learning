67349eff0cb80910ea5c53d765ef781b71a270b4
wallacezhang <wallace.zhang@fjdynamics.com>
2024-05-10 20:33:49 +0800

    建立新工程

---
+++ code_CAN_Comm/components/BSP/TIMER/simple_timer.h
@@ -0,0 +1,14 @@
+#ifndef _SIMPLE_TIMER_H_
+#define _SIMPLE_TIMER_H_
+
+#include "stdbool.h"
+#include "stdio.h"
+#include "string.h"
+
+void timer_Init(void);
+void timer_Start(void);
+
+
+
+#endif
+
