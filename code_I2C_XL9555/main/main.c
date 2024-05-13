cdf595cc511d8d128adaf21566446b3e03f74281
wallacezhang <wallace.zhang@fjdynamics.com>
2024-05-13 11:11:06 +0800

    建立新工程

---
+++ code_I2C_XL9555/main/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "led.h"
+#include "can_comm.h"
+
+/**
+ * @brief 主函数
+ * 
+ */
+void app_main(void)
+{
+    led_Init();       // 初始化LED
+    can_Comm_Init2(); // 初始化CAN控制器、方案2
+    while(1) {
+        esp_log_write(ESP_LOG_INFO,"MAIN","%s(%d): can comm\n",__FUNCTION__, __LINE__);
+        led_Toggle();
+        CAN_Receive_Msg_And_Send_Msg();
+        vTaskDelay(500 / portTICK_PERIOD_MS);    
+    }
+}
+
