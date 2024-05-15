edcde90a233954d4c59a0f21449082cbcf46b788
wallacezhang <wallace.zhang@fjdynamics.com>
2024-05-15 12:07:51 +0800

    解决找不到esp_now.h头文件的问题；

---
+++ code_ESP_NOW_Comm/components/BSP/ESP_NOW_COMM/sender.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_now.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
