65abb8c94cf21e90a13f6b0249273a2fbe3f47b3
wallacezhang <wallace.zhang@fjdynamics.com>
2024-05-27 21:17:35 +0800

    code_arduino01项目创建

---
+++ code_arduino01/components/BSP/TIMER/general_purpose_timer.h
@@ -0,0 +1,15 @@
+#ifndef _GENERAL_PURPOSE_TIMER_H_
+#define _GENERAL_PURPOSE_TIMER_H_
+
+#include "stdbool.h"
+#include "stdio.h"
+#include "string.h"
+
+
+void init_And_Start_GPtimer1(void);
+uint64_t get_Gptimer_CNT(void);
+
+
+#endif
+
+
